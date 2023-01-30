09c26edcea74778f4fb65a1773e833f4c31f5039
Johanneslueke <Johanneslueke@users.noreply.github.com>
2023-01-30 18:57:55 +0100

    2 files modified

--- src/app/navigation/navigation-page/navigation-page.component.ts
+++ src/app/navigation/navigation-page/navigation-page.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navigation-page',
   templateUrl: './navigation-page.component.html',
-  styleUrls: ['./navigation-page.component.css']
+  styleUrls: ['./navigation-page.component.css'],
 })
 export class NavigationPageComponent implements OnInit {
+  links = [
+    {
+      path: 'List',
+      label: 'Todos',
+      icon: '',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
