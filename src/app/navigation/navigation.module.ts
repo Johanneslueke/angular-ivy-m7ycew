0e32ef33494e2fa3f58256326861d3d3b6e51b3e
Johanneslueke <Johanneslueke@users.noreply.github.com>
2023-01-30 18:24:44 +0100

    2 files modified, 9 added

--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -3,10 +3,22 @@ import { CommonModule } from '@angular/common';
 import { NavigationRoutingModule } from './navigation-routing/navigation-routing.module';
 import { NavigationPageComponent } from './navigation-page/navigation-page.component';
 import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
+import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule, NavigationRoutingModule],
   exports: [NavigationPageComponent],
-  declarations: [NavigationPageComponent],
+  declarations: [
+    NavigationPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainContentComponent,
+    LeftSidebarComponent,
+    RightSidebarComponent,
+  ],
 })
 export class NavigationModule {}
