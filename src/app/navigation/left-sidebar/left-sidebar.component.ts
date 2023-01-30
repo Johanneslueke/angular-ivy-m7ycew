fd5b8e310598cd1abba992aad150112a6c3d4b17
Johanneslueke <Johanneslueke@users.noreply.github.com>
2023-01-30 18:50:54 +0100

    2 files modified

--- src/app/navigation/left-sidebar/left-sidebar.component.ts
+++ src/app/navigation/left-sidebar/left-sidebar.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-left-sidebar',
   templateUrl: './left-sidebar.component.html',
-  styleUrls: ['./left-sidebar.component.css']
+  styleUrls: ['./left-sidebar.component.css'],
 })
 export class LeftSidebarComponent implements OnInit {
+  private _mobileQueryListener: () => void;
+  mobileQuery: MediaQueryList;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addEventListener('change', this._mobileQueryListener);
   }
 
+  ngOnDestroy(): void {
+    this.mobileQuery.removeAllListeners();
+  }
+
+  ngOnInit() {}
 }
